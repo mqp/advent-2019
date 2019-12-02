c84e08b8f7a27c2d87088850202838fe4df312b9
Marshall Polaris <marshall@pol.rs>
2019-12-01 22:41:35 -0800

    Slight tidying in anticipation of future tasks

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,50 +1,54 @@
 use std::error::Error;
-use std::io;
-use std::io::Read;
+use std::io::{self, Read};
 use std::num::ParseIntError;
 
 fn parse(input: &str) -> Result<Vec<usize>, ParseIntError> {
     input.split(',').map(|s| s.parse()).collect()
 }
 
-fn execute(memory: &mut [usize]) {
-    let mut pc = 0;
-    loop {
-        let opcode = memory[pc];
-        match opcode {
-            1 => { // add
-                let noun = memory[pc+1];
-                let verb = memory[pc+2];
-                let dest = memory[pc+3];
-                memory[dest] = memory[noun] + memory[verb];
-                pc += 4;
-            }
-            2 => { // mul
-                let noun = memory[pc+1];
-                let verb = memory[pc+2];
-                let dest = memory[pc+3];
-                memory[dest] = memory[noun] * memory[verb];
-                pc += 4;
-            }
-            99 => { // halt
-                return;
-            }
-            _ => {
-                panic!(format!("Fishy opcode found: {}", opcode));
-            }
+fn exec(memory: &mut [usize], opcode: usize, pc: &mut usize) {
+    return match opcode {
+        1 => { // add
+            let noun = memory[*pc+1];
+            let verb = memory[*pc+2];
+            let dest = memory[*pc+3];
+            memory[dest] = memory[noun] + memory[verb];
+            *pc += 4;
+        }
+        2 => { // mul
+            let noun = memory[*pc+1];
+            let verb = memory[*pc+2];
+            let dest = memory[*pc+3];
+            memory[dest] = memory[noun] * memory[verb];
+            *pc += 4;
+        }
+        _ => {
+            panic!(format!("Fishy opcode found: {}", opcode));
         }
     }
 }
 
-fn search_target(memory: &[usize], target: usize) -> (usize, usize) {
+fn run(memory: &mut [usize]) {
+    let mut pc = 0;
+    loop {
+        match memory[pc] {
+            99 => { return; } // halt
+            op => exec(memory, op, &mut pc)
+        }
+    }
+}
+
+fn search_until<F>(memory: &[usize], predicate: F) -> (usize, usize) where F: Fn(&[usize]) -> bool {
+    let mut state = memory.to_vec();
     for noun in 0.. {
         for verb in 0..noun {
-            let mut state = memory.to_owned();
             state[1] = noun;
             state[2] = verb;
-            execute(&mut state);
-            if state[0] == target {
+            run(&mut state);
+            if predicate(&state) {
                 return (noun, verb)
+            } else {
+                state.copy_from_slice(memory);
             }
         }
     }
@@ -55,6 +59,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
     let memory = parse(input.trim_end())?;
-    println!("Result: {:?}", search_target(&memory, 19690720));
+    println!("Result: {:?}", search_until(&memory, |s| s[0] == 19690720));
     Ok(())
 }
