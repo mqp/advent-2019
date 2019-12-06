7409e9bc902f30b4f481a8093bb7399d055ec515
Marshall Polaris <marshall@pol.rs>
2019-12-05 21:44:36 -0800

    Day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,48 @@
+use std::error::Error;
+use std::io::{self, Read};
+use std::collections::HashMap;
+use std::collections::vec_deque::VecDeque;
+
+fn parse_orbit<'a>(s: &'a str) -> Result<(&'a str, &'a str), Box<dyn Error>> {
+    let mut parts = s.split(")");
+    let inner = parts.next().ok_or("Invalid orbit.")?;
+    let outer = parts.next().ok_or("Invalid orbit.")?;
+    Ok((inner, outer))
+}
+
+fn parse_graph<'a>(input: &'a str) -> Result<HashMap<&'a str, &'a str>, Box<dyn Error>> {
+    let mut graph = HashMap::new();
+    for orbit in input.split_whitespace().map(parse_orbit) {
+        let (inner, outer) = orbit?;
+        graph.insert(outer, inner);
+    }
+    Ok(graph)
+}
+
+fn get_path<'a>(graph: &'a HashMap<&'a str, &'a str>, outer: &'a str, target: &'a str) -> VecDeque<&'a str> {
+    let mut result = VecDeque::new();
+    let mut curr = outer;
+    while let Some(&parent) = graph.get(curr) {
+        result.push_front(parent);
+        if parent == target {
+            return result;
+        }
+        curr = parent;
+    }
+    panic!(format!("No path from {} to {}!", outer, target));
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    let graph = parse_graph(&input)?;
+    let mut com_to_you = get_path(&graph, "YOU", "COM");
+    let mut com_to_san = get_path(&graph, "SAN", "COM");
+    loop {
+        if com_to_you.pop_front() != com_to_san.pop_front() {
+            break;
+        }
+    }
+    println!("Result: {}", com_to_you.len() + com_to_san.len() + 2); // +2 because we popped one extra level
+    Ok(())
+}
